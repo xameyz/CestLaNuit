50cab53f3895026341a189764a90f12bb36c75dd
douceurnocturne <xzaitsevx@gmail.com>
2017-01-18 16:35:15 +0100

    Add files via upload

---
+++ projetUML-V1/src/Image.java
@@ -0,0 +1,29 @@
+import java.util.Dictionary;
+
+public class Image {
+	private double coordonnees;
+	private String nom;
+	private String [] commentaire;
+	
+	
+	
+	public Image(){
+		
+	
+	}
+	public void AddCommentaire(){
+		
+	}
+	public void AfficherImage(){
+		
+	}
+	public double GetCoorIm(){
+		return this.coordonnees;
+	}
+	public String GetNomIm(){
+		return this.nom;
+	}
+	public String[] GetCommIm(){
+		return this.commentaire;
+	}
+}
---
+++ projetUML-V1/src/Localisation.java
@@ -0,0 +1,22 @@
+
+public class Localisation {
+	private double coordonnees;
+	private String nom;
+	
+	
+	public Localisation(){
+		
+	}
+	public void Favoriser(){
+		
+	}
+	public void Selectionner(){
+		
+	}
+	public String GetNomL(){
+		return this.nom;
+	}
+	public double GetCoorL(){
+		return this.coordonnees;
+	}
+}
