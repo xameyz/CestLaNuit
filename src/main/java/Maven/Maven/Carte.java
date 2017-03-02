4fd89b17b2ea742dd428169b93efd501a0e82cca
Alexandre Badez <alexandre.badez@gmail.com>
2017-03-02 22:55:41 +0100

    Je pointe.

--- src/main/java/Maven/Maven/Carte.java
+++ src/main/java/Maven/Maven/Carte.java
@@ -2,33 +2,36 @@ package Maven.Maven;
 
 import java.awt.Component;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.jxmapviewer.JXMapKit;
 import org.jxmapviewer.viewer.DefaultWaypoint;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.Waypoint;
 
 public class Carte {
 	private JXMapKit map;
-	private List<DefaultWaypoint> list_waypoints;
+	private Set<DefaultWaypoint> waypoints;
 	private CustomPainter painter;
 
 	public Carte() {
-		this.list_waypoints = new ArrayList<DefaultWaypoint>();
-		list_waypoints.add(new DefaultWaypoint(51.50, -0.12));
+		this.waypoints = new HashSet<DefaultWaypoint>();
+		this.waypoints.add(new DefaultWaypoint(51.50, -0.12));
 
 		this.map = new JXMapKit();
 		this.map.setDefaultProvider(JXMapKit.DefaultProviders.OpenStreetMaps);
 
 		this.painter = new CustomPainter();
 
+		this.map.getMainMap().setOverlayPainter(painter);
 		Component mouseClick = new MyComponent(this);
 		this.map.getMainMap().addMouseListener(((MouseListener) mouseClick));
 	}
 
 	public void paint() {
-		this.painter.setWaypoints(list_waypoints);
-		this.map.getMainMap().setOverlayPainter(painter);
+		this.painter.setWaypoints(waypoints);
 		this.map.getMainMap().repaint();
 	}
 
@@ -37,11 +40,16 @@ public class Carte {
 	}
 
 	public void addWaypoint(double xpx, double ypx) {
-		this.list_waypoints.add(new DefaultWaypoint(xpx, ypx));
+		this.waypoints.add(new DefaultWaypoint(xpx, ypx));
 		this.paint();
 	}
 
-	public List<DefaultWaypoint> getListe() {
-		return this.list_waypoints;
+	public Collection<? extends Waypoint> getWayPoint() {
+		return waypoints;
+	}
+
+	public void addWaypoint(GeoPosition point) {
+		this.waypoints.add(new DefaultWaypoint(point));
+		this.paint();
 	}
 }
