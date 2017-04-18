4e57dc2aa20177f49c6ab5d68c134564feb83877
pineapplelananas <fd.flo.denise@gmail.com>
2017-04-18 21:23:06 +0200

    bouton ajouter image fonctionel

--- src/main/java/Maven/Maven/Principal.java
+++ src/main/java/Maven/Maven/Principal.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,12 +15,11 @@ import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
 
-public class Principal extends JFrame{
+public class Principal extends JFrame implements ActionListener {
 
-	static Map current_map ;
+	static Map current_map;
 
-
-	private JButton  buttonAdd = new JButton("Ajouter Image");
+	private JButton buttonAdd = new JButton("Ajouter Image");
 	private JButton buttonExit = new JButton("Quitter");
 
 	private JTabbedPane panel_R = new JTabbedPane();
@@ -28,19 +30,19 @@ public class Principal extends JFrame{
 
 	private JScrollPane panel_scroll_l = new JScrollPane(panel_L);
 
-
-	public Principal(){
+	public Principal() throws IOException {
 
 		// map
 		current_map = new Map();
 
-		// Panel de du bas avec des boutons
+		// Panel du bas avec des boutons
 
 		panel_Down.setLayout(new FlowLayout());
 		panel_Down.setBackground(Color.blue);
 		panel_Down.setPreferredSize(new Dimension(800, 50));
 		panel_Down.add(buttonExit);
 		panel_Down.add(buttonAdd);
+		buttonAdd.addActionListener(this);
 
 		// panel de gauche
 		panel_L.setLayout(new FlowLayout());
@@ -55,8 +57,6 @@ public class Principal extends JFrame{
 		panel_R.setBackground(Color.lightGray);
 		panel_R.setPreferredSize(new Dimension(200, 300));
 
-
-
 		// frame
 
 		add(current_map.getMap());
@@ -69,17 +69,35 @@ public class Principal extends JFrame{
 		setLocationRelativeTo(null);
 
 	}
-	public static void main(String[] args){
 
-//java.awt.EventQueue. ou  SwingUtilities
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object source = e.getSource();
+
+		if (source == buttonAdd) {
+			try {
+				this.current_map.addImg();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+
+		// java.awt.EventQueue. ou SwingUtilities
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				new Principal().setVisible(true);
+				try {
+					new Principal().setVisible(true);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 	}
 
-
-
 }
-
