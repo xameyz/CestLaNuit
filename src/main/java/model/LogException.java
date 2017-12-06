5a4534a77bdb52d67e8a285888dc211a34b343fa
mecalle <mecalle@pcalb-mm0318>
2017-12-06 15:53:58 +0100

    Initialisation

---
+++ src/main/java/model/LogException.java
@@ -0,0 +1,10 @@
+package model;
+
+// Une classe d'exceptions spécifiques pour le DAO
+public class LogException extends Exception {
+
+    public LogException(String message) {
+        super(message);
+    }
+
+}
